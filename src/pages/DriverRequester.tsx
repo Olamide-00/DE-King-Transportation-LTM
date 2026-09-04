dd5f625722eb51eca91d771b64b2d06589fb34f1
Olamide Oladele <officialolamide001@gmail.com>
2026-09-04 05:44:02 +0100

    fixed

--- src/pages/DriverRequester.tsx
+++ src/pages/DriverRequester.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import RoadCanvas from "../components/RoadCanvas";
 import Reveal from "../components/Reveal";
 import WhatsAppCTA from "../components/WhatsAppCTA";
